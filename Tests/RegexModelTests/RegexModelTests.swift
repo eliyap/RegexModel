761c6029d97e882148d4fec2857ed8370540f380
eliyap <yap@usc.edu>
2022-07-01 06:34:08 -0400

    remove example code

--- Tests/RegexModelTests/RegexModelTests.swift
+++ Tests/RegexModelTests/RegexModelTests.swift
@@ -5,13 +5,6 @@ final class RegexModelTests: XCTestCase {
     
     var pattern: Regex<Substring>! = nil
     
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RegexModel().text, "Hello, World!")
-    }
-    
     func testOneOrMore() throws {
         pattern = ComponentModel.oneOrMore(.init(components: [
             .string(.init(string: "cat")),
