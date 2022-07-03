8dd7aa9133de7fd6eeef07aa9037bf7b6e737cc1
eliyap <yap@usc.edu>
2022-07-03 18:26:52 -0400

    declare section organization

--- Sources/RegexModel/Section.swift
+++ Sources/RegexModel/Section.swift
@@ -24,17 +24,6 @@ public extension ComponentModel {
         /// Color for blocks representing other behaviours.
         case other
     }
-    
-    var section: Section {
-        switch self {
-        case .string, .anchor:
-            return .text
-        case .zeroOrMore, .oneOrMore, .optionally, .repeat:
-            return .quantifier
-        case .lookahead, .choiceOf:
-            return .other
-        }
-    }
 }
 
 public extension Color {
@@ -51,4 +40,43 @@ public extension Color {
     
     /// Color for blocks representing other behaviours.
     static var other: Color { .blue }
+}
+
+public extension ComponentModel {
+    /// Represents the models without the associated values, for table purposes.
+    enum Proxy: Int, CaseIterable, Codable {
+        /// Text
+        case string     = 0
+        case anchor     = 1
+        
+        /// Quantifiers
+        case zeroOrMore = 2
+        case oneOrMore  = 3
+        case optionally = 4
+        case `repeat`   = 5
+        
+        /// Others
+        case lookahead  = 6
+        case choiceOf   = 7
+    }
+}
+
+extension ComponentModel.Proxy {
+    var section: ComponentModel.Section {
+        switch self {
+        case .string, .anchor:
+            return .text
+        case .zeroOrMore, .oneOrMore, .optionally, .repeat:
+            return .quantifier
+        case .lookahead, .choiceOf:
+            return .other
+        }
+    }
+}
+
+extension ComponentModel.Section {
+    /// Array type guarantees a stable order, specifically declaration order.
+    var proxyItems: [ComponentModel.Proxy] {
+        ComponentModel.Proxy.allCases.filter { $0.section == self }
+    }
 }
